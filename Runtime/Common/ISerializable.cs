01f7a9c6b43824cafe5a23bc8d792e78ce356606
sindydwns <sindydwns@naver.com>
2026-03-10 17:36:24 +0900

    feat: add ISerializable interface for serialization and deserialization

---
+++ Runtime/Common/ISerializable.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace Sindy.Core
+{
+    public interface ISerializable<KeyType, ValueType>
+    {
+        /// <summary>
+        /// Serializes the object to a string.
+        /// </summary>
+        /// <returns>A string representation of the serialized object.</returns>
+        JContainer Serialize();
+
+        /// <summary>
+        /// Deserializes the object from a string.
+        /// </summary>
+        /// <param name="data">The string data to deserialize from.</param>
+        /// <param name="refs">A dictionary of references to resolve any dependencies during deserialization.</param>
+        void Deserialize(JContainer data, Dictionary<KeyType, ValueType> refs);
+    }
+}
