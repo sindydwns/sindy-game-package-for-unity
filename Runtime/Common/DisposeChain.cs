eebccff702f23d17c710323760700cb93e7c20e4
sindydwns <sindydwns@naver.com>
2026-03-10 18:05:19 +0900

    feat: implement DisposeChain and IDisposeChain for resource management

---
+++ Runtime/Common/DisposeChain.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+
+namespace Sindy.Core
+{
+    public class DisposeChain : IDisposeChain
+    {
+        private readonly object _lock = new object();
+        private readonly List<IDisposeChain> _children = new();
+        private bool _isDisposed;
+
+        public void AddTo(IDisposeChain disposable)
+        {
+            if (disposable == null || ReferenceEquals(this, disposable))
+            {
+                return;
+            }
+
+            disposable.AddChild(this);
+        }
+
+        public void AddChild(IDisposeChain child)
+        {
+            if (child == null || ReferenceEquals(this, child))
+            {
+                return;
+            }
+
+            var shouldDisposeImmediately = false;
+
+            lock (_lock)
+            {
+                if (_isDisposed)
+                {
+                    shouldDisposeImmediately = true;
+                }
+                else
+                {
+                    _children.Add(child);
+                }
+            }
+
+            if (shouldDisposeImmediately)
+            {
+                child.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            List<IDisposeChain> childrenToDispose;
+
+            lock (_lock)
+            {
+                if (_isDisposed)
+                {
+                    return;
+                }
+
+                _isDisposed = true;
+                childrenToDispose = new(_children);
+                _children.Clear();
+            }
+
+            foreach (var child in childrenToDispose)
+            {
+                child?.Dispose();
+            }
+        }
+    }
+}
---
+++ Runtime/Common/IDisposeChain.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Sindy.Core
+{
+    public interface IDisposeChain : IDisposable
+    {
+        // a.AddTo(b) 하면 a가 b에 종속되어, b가 Dispose 될 때 a도 함께 Dispose 된다.
+        public void AddTo(IDisposeChain disposable);
+
+        // a.AddChild(b) 하면 a가 b의 부모가 되어, a가 Dispose 될 때 b도 함께 Dispose 된다.
+        public void AddChild(IDisposeChain child);
+    }
+}
