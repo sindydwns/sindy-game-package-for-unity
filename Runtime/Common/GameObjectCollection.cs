47ba9008d0ae861702330beaae0019e5498ddef7
sindydwns <sindydwns@naver.com>
2026-04-11 02:30:26 +0900

    perf(Common): GameObjectCollection 딕셔너리 캐싱
    O(n) 선형 탐색을 최초 접근 시 구축되는 Dictionary 캐시로 O(1)로 개선.

--- Runtime/Common/GameObjectCollection.cs
+++ Runtime/Common/GameObjectCollection.cs
@@ -6,16 +6,29 @@ namespace Sindy.Common
     public class GameObjectCollection : MonoBehaviour, IGameObjectCollection
     {
         [SerializeField] private List<GameObject> prefabs = new();
+        private Dictionary<string, GameObject> _cache;
+
+        private Dictionary<string, GameObject> Cache
+        {
+            get
+            {
+                if (_cache == null)
+                {
+                    _cache = new Dictionary<string, GameObject>(prefabs.Count);
+                    foreach (var prefab in prefabs)
+                    {
+                        if (prefab != null)
+                            _cache[prefab.name] = prefab;
+                    }
+                }
+                return _cache;
+            }
+        }
 
         public T GetGameObject<T>(string name) where T : Object
         {
-            foreach (var prefab in prefabs)
-            {
-                if (prefab != null && prefab.name.Equals(name))
-                {
-                    return prefab.GetComponent<T>();
-                }
-            }
+            if (Cache.TryGetValue(name, out var prefab))
+                return prefab.GetComponent<T>();
             return null;
         }
     }
